ac5b4e7038373102990a05b5e3b1a832cd4104e0
Igor <exemple@gmail.com>
2022-10-27 17:04:20 +0300

    remake remake componen App on hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react/cjs/react.development";
+import { useState } from "react";
 
 import AppHeader from "./AppHeader";
 import RandomChar from "./RandomChar";
@@ -8,40 +8,35 @@ import ErrorBoundary from "./ErrorBoundary";
 
 import decoration from '../resources/img/vision.png';
 
-class App extends Component {
-    state = { 
-        selectedChar: null  //id персонажа, для отображения комиксов в CharInfo
-    }
+const App = () => {
+
+    const [selectedChar, setChar] = useState(null) //id персонажа, для отображения комиксов в CharInfo
 
     // метод устанавливает state selectedChar через аргумент id. (Подъем состояния с другого компонента)
-    onCharSelected = (id) => {
-        this.setState({
-            selectedChar: id
-        })
+    const onCharSelected = (id) => {
+        setChar(id)
     }
-    // done
-    render() {
-        return (
-            <div className="app">
-                <AppHeader/>
-                <main>
+
+    return (
+        <div className="app">
+            <AppHeader/>
+            <main>
+                <ErrorBoundary>
+                    <RandomChar/>
+                </ErrorBoundary>
+                <div className="char__content">
                     <ErrorBoundary>
-                        <RandomChar/>
+                        <CharList onCharSelected={onCharSelected}/>    {/*получаем id и записываем в state*/}
                     </ErrorBoundary>
-                    <div className="char__content">
-                        <ErrorBoundary>
-                            <CharList onCharSelected={this.onCharSelected}/>    {/*получаем id и записываем в state*/}
-                        </ErrorBoundary>
-                        <ErrorBoundary>
-                           <CharInfo charId={this.state.selectedChar}/>    {/*передаем полученый id от CharList*/}
-                        </ErrorBoundary>
-                    </div>
-                    <img className="bg-decoration" src={decoration} alt="vision"/>
-                </main> 
-                
-            </div>
-        )
-    }
+                    <ErrorBoundary>
+                        <CharInfo charId={selectedChar}/>    {/*передаем полученый id от CharList*/}
+                    </ErrorBoundary>
+                </div>
+                <img className="bg-decoration" src={decoration} alt="vision"/>
+            </main> 
+            
+        </div>
+    )
 }
 
 
