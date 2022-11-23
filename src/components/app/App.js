63cc4de693b1eb750c8928452cffbc1be842a69d
Igor <exemple@gmail.com>
2022-11-23 13:03:53 +0200

    all errors fixed

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -16,8 +16,8 @@ const App = () => {
                         <Routes>
                             <Route path="/" element={<MainPage/>}/>
                             <Route path="/comics" element={<ComicsPage/>}/>
-                            <Route path="/comics/:id" element={<SinglePage Component={<SingleComicLayout/>}  dataType='comic'/>}/>
-                            <Route path="/characters/:id" element={<SinglePage Component={<SingleCharacterLayout/>}  dataType='character'/>} />
+                            <Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout}  dataType='comic'/>}/>
+                            <Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout}  dataType='character'/>} />
                             <Route path="*" element={<Page404/>}/>
                         </Routes>
                     </main> 
